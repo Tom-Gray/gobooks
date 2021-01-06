d43159047caed827bf4e7bae3afe751333f0b6c8
Tom <swinkistom@gmail.com>
2021-01-06 14:45:33 +1100

    first commitI can write and read to the DB

--- cmd/books.go
+++ cmd/books.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type book struct {
@@ -11,6 +14,7 @@ type book struct {
 }
 
 func newBook(title string, author string, rating int) *book {
+	//whats the best way to test for empty\incorrect values?
 	b := book{title: title}
 	b.author = author
 	b.rating = rating
@@ -35,7 +39,29 @@ func addBook() error {
 
 }
 
+//interact with database
 func writeEntry(book *book) error {
+	database, _ := sql.Open("sqlite3", "./book.db")
+	createDbStatement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, rating INTEGER)")
+	createDbStatement.Exec()
 	fmt.Println("Writing this book:", book)
+	statement, _ := database.Prepare("INSERT INTO books (title, author, rating) VALUES (?, ?, ?)")
+	statement.Exec(book.title, book.author, book.rating)
+
+	return nil
+}
+
+func listAllEntries() error {
+	database, _ := sql.Open("sqlite3", "./book.db")
+	rows, _ := database.Query("SELECT id, title, author, rating FROM books")
+	var id int
+	var title string
+	var author string
+	var rating int
+
+	for rows.Next() {
+		rows.Scan(&id, &title, &author, &rating)
+		fmt.Printf("Book No. %v: Title: %v \n Author: %v \n Rating: %v", id, title, author, rating)
+	}
 	return nil
 }
--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ var CLI struct {
 		Author string `help:"Who wrote it"`
 		Rating int    `help:"How many stars?"`
 	} `cmd help:"Add a book"`
+	List struct{} `cmd help:"list all the books in the db"`
 }
 
 func main() {
@@ -25,6 +26,8 @@ func mainCore() error {
 	switch ctx.Command() {
 	case "add":
 		return addBook()
+	case "list":
+		return listAllEntries()
 
 	default:
 		panic(ctx.Command())
